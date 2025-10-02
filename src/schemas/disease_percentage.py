4af3a6c2b2af9dfb42418143b97d6c5345a5f46f
lonewanderer27 <adrianejames27@gmail.com>
2025-10-03 02:23:51 +0800

    docs(disease_percentage): refactor the doc

--- src/schemas/disease_percentage.py
+++ src/schemas/disease_percentage.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel, RootModel
+from pydantic import RootModel, ConfigDict
 from typing import Dict
 
 from src.enums.disease_type import DiseaseType
 
 
 class DiseasePercentageMap(RootModel[Dict[DiseaseType, int]]):
-    class Config:
-        json_schema_extra = {
+    model_config = ConfigDict(
+        json_schema_extra={
             "example": {
                 DiseaseType.BlackSigatoka.value: 80,
                 DiseaseType.BBTV.value: 60,
                 DiseaseType.Panama.value: 30,
             }
         }
+    )
