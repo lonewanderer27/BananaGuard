5b6fe00620a310a28f4c2cad8f1511ba4f172b24
lonewanderer27 <adrianejames27@gmail.com>
2025-10-03 02:28:13 +0800

    feat: create insight response model

---
+++ src/schemas/insight_response.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+
+
+class InsightResponse(BaseModel):
+    conversation_id: str = Field(
+        ...,
+        examples=["86Rf07"]
+    ),
+    answer: str = Field(
+        ...,
+        examples=[
+            "BBTV (Banana Bunchy Top Virus) causes plants to grow upright with narrow, brittle leaves and reduced fruit production. There is no cure, but infected plants should be destroyed and removed to prevent spread. Control banana aphids, which transmit the virus, and plant only certified virus-free seedlings."
+        ]
+    )
