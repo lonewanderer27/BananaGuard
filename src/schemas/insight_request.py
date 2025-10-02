8fd529cd3288e6a13836480c65bdd05745939757
lonewanderer27 <adrianejames27@gmail.com>
2025-10-03 02:18:47 +0800

    docs(insight_request): add example doc

--- src/schemas/insight_request.py
+++ src/schemas/insight_request.py
@@ -1,11 +1,25 @@
 from typing import Dict, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from src.enums.disease_type import DiseaseType
 
 
 class InsightRequest(BaseModel):
-    analysis: Dict[DiseaseType, int]
-    inquiry: Optional[str] = None
-    conversation_id: Optional[str] = None
+    analysis: Dict[DiseaseType, int] = Field(
+        examples=[
+            {
+                DiseaseType.BlackSigatoka.value: 80,
+                DiseaseType.BBTV.value: 60,
+                DiseaseType.Panama.value: 30
+            }
+        ]
+    )
+    inquiry: Optional[str] = Field(
+        None,
+        examples=["Tell me more about this disease and how can it be treated?"]
+    )
+    conversation_id: Optional[str] = Field(
+        None,
+        examples=["86Rf07"]
+    )
