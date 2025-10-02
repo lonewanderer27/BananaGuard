efad0a7cfa52c0b4e11e6bdb302de787c218f45c
lonewanderer27 <adrianejames27@gmail.com>
2025-10-03 02:39:58 +0800

    feat(insights): type api route and return sample data

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 from src.routers import detect
+from src.routers import insights
 
-bananaAPI = FastAPI(title="Banana API", version="0.1")
+bananaAPI = FastAPI(title="BananaGuard API", version="0.1")
 
 @bananaAPI.get('/')
 async def index():
     return "Welcome to BananaGuard API. Please visit the documentation at /docs for more information."
 
 bananaAPI.include_router(detect.route)
+bananaAPI.include_router(insights.route)
 
 if __name__ == "__main__":
     uvicorn.run(bananaAPI, host="0.0.0.0", port=8000)
