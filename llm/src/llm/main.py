e70c6366b55682f4f5c1f76eb7ba19894f56c327
lonewanderer27 <adrianejames27@gmail.com>
2025-10-25 18:26:50 +0800

    feat(llm): add basic return text for root route

--- llm/src/llm/main.py
+++ llm/src/llm/main.py
@@ -5,5 +5,9 @@ from llm.routers.rag import rag_route
 bananaLlm = FastAPI(title="BananaGuard LLM", version="0.1")
 bananaLlm.include_router(rag_route)
 
+@bananaLlm.get('/')
+async def index():
+    return "Welcome to BananaGuard LLM. Please visit the documentation at /docs for more information."
+
 if __name__ == "__main__":
     uvicorn.run(bananaLlm, host="0.0.0.0", port=8001)
