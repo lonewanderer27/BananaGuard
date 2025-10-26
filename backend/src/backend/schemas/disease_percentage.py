ce5b8542a440126d01fbbf5af3802c22514ddf29
lonewanderer27 <adrianejames27@gmail.com>
2025-10-26 12:36:23 +0800

    fix(backend): correct disease percentage types and update example values in DiseasePercentageMap

--- backend/src/backend/schemas/disease_percentage.py
+++ backend/src/backend/schemas/disease_percentage.py
@@ -4,16 +4,16 @@ from typing import Dict
 from backend.enums.disease_type import DiseaseType
 
 
-class DiseasePercentageMap(RootModel[Dict[DiseaseType, int]]):
+class DiseasePercentageMap(RootModel[Dict[DiseaseType, float]]):
     model_config = ConfigDict(
         json_schema_extra={
             "example": {
-                DiseaseType.BlackSigatoka.value: 80,
-                DiseaseType.BBTV.value: 60,
+                DiseaseType.BBTV.value: 80,
+                DiseaseType.Cordana.value: 60,
                 DiseaseType.Panama.value: 30,
-                DiseaseType.Cordana.value: 17,
+                DiseaseType.Healthy.value: 17,
                 DiseaseType.Pestalotiopsis.value: 10,
-                DiseaseType.Healthy.value: 3
+                DiseaseType.BlackSigatoka.value: 3
             }
         }
     )
