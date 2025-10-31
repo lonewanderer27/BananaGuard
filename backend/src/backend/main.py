73b9d68b4c36fa91a88efff852c7086f0016382b
lonewanderer27 <adrianejames27@gmail.com>
2025-10-31 17:19:12 +0800

    chore(backend): remove useless insights route

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,7 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
 from backend.routers import detect
-from backend.routers import insights
 from backend.services.load_model import load_model
 from contextlib import asynccontextmanager
 
@@ -22,7 +21,6 @@ async def index():
     return "Welcome to BananaGuard API. Please visit the documentation at /docs for more information."
 
 bananaAPI.include_router(detect.route)
-bananaAPI.include_router(insights.route)
 
 if __name__ == "__main__":
     uvicorn.run(bananaAPI, host="0.0.0.0", port=8000)
