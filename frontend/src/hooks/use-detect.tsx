78b8d616f8a81373ee535fb6f7441f2c63404ce9
lonewanderer27 <adrianejames27@gmail.com>
2025-10-31 17:12:37 +0800

    fix(frontend): use insight and use detect hooks

--- frontend/src/hooks/use-detect.tsx
+++ frontend/src/hooks/use-detect.tsx
@@ -1,18 +1,22 @@
 import { useMutation } from "@tanstack/react-query";
-import { BackendServices } from '@/services/backend.service';
-import { AnalysisResult } from "@/types/analysis-result.types";
-import { analysisResultAtom } from "@/atoms";
 import { useSetAtom } from "jotai";
 
-const setAnalysisResult = useSetAtom(analysisResultAtom);
+import { BackendServices } from "@/services/backend.service";
+import { AnalysisResult } from "@/types/analysis-result.types";
+import { analysisResultAtom } from "@/atoms";
 
-const useDetect = useMutation({
-  mutationFn: async (photo: File) => {
-    return BackendServices.detect(photo);
-  },
-  onSuccess: (data: AnalysisResult) => {
-    setAnalysisResult(data);
-  }
-})
 
-export default useDetect
+const useDetect = () => {
+  const setAnalysisResult = useSetAtom(analysisResultAtom);
+
+  return useMutation({
+    mutationFn: async (photo: File) => {
+      return BackendServices.detect(photo);
+    },
+    onSuccess: (data: AnalysisResult) => {
+      setAnalysisResult(data);
+    },
+  });
+}
+
+export default useDetect;
--- frontend/src/hooks/use-insight.tsx
+++ frontend/src/hooks/use-insight.tsx
@@ -1,20 +1,30 @@
-import { insightResultAtom } from '@/atoms'
-import { LLMServices } from '@/services/llm.service.ts'
-import { AnalysisResult } from '@/types/analysis-result.types'
-import { useMutation } from '@tanstack/react-query'
-import { useSetAtom } from 'jotai'
+import { useMutation } from "@tanstack/react-query";
+import { useSetAtom } from "jotai";
 
-const setInsightResult = useSetAtom(insightResultAtom);
+import { insightResultAtom } from "@/atoms";
+import { LLMServices } from "@/services/llm.service.ts";
+import { AnalysisResult } from "@/types/analysis-result.types";
 
-const useInsight = useMutation({
-  mutationFn: async ({
-    question, analysisResult, sources
-  }: { question: string, analysisResult: AnalysisResult, sources: boolean }) => {
-    return LLMServices.insight(question, sources, analysisResult);
-  },
-  onSuccess: (data: InsightResponse) => {
-    setInsightResult(data);
-  }
-})
 
-export default useInsight
+const useInsight = () => {
+  const setInsightResult = useSetAtom(insightResultAtom);
+
+  return useMutation({
+    mutationFn: async ({
+      question,
+      analysisResult,
+      sources,
+    }: {
+      question: string;
+      analysisResult: AnalysisResult;
+      sources: boolean;
+    }) => {
+      return LLMServices.insight(question, sources, analysisResult);
+    },
+    onSuccess: (data: InsightResponse) => {
+      setInsightResult(data);
+    },
+  });
+}
+
+export default useInsight;
