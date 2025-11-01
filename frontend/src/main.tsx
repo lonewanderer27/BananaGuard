59f8de8a7f5e294e018016f015685413f68a4a69
lonewanderer27 <adrianejames27@gmail.com>
2025-11-01 19:44:22 +0800

    fix(frontend): setup query client provider in the root of the app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,19 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// create a query client
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
